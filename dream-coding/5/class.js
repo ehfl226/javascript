3e2f71cf0adf2e2121bf363eed45c4e951c03107
이지윤 <ehfl226@gmail.com>
2023-12-14 23:52:37 +0900

    class

--- dream-coding/5/class.js
+++ dream-coding/5/class.js
@@ -49,3 +49,89 @@ class User {
 
 const user1 = new User('Steve', 'Job', -1);
 console.log(user1.age);
+
+
+// 3. Fields (public, private)
+// Too soon!
+class Experiment {
+    publicField = 2;
+    #privateField = 0;
+}
+const Experiment = new Experiment();
+console.log(experiment.publicField);
+console.log(experiment.privateField);
+
+
+// 4. Static properties and methods
+// Too soon!
+class Article {
+    static publisher = 'Dream Coding';
+    constructor(articleNumber) {
+        this.articleNumber = articleNumber;
+    }
+
+    static printPublisher() {
+      console.log(Article.publisher);
+    }
+
+    static printPublisher() {
+      console.log(Article.publisher);
+    }
+}
+
+const article1 = new Article(1);
+const article2 = new Article(2);
+console.log(Article.publisher);
+Article.printPublisher();
+
+
+// 5. Inheritance
+// a way for one class to extend another class.
+class Shape {
+  constructor(width, heigth, color) {
+    this.width = width;
+    this.heigth = heigth;
+    this.color = color;
+  }
+
+  draw() {
+    console.log(`drawing ${this.color} color !`);
+  }
+
+  getArea() {
+    return width * this.heigth;
+  }
+}
+
+class Rectangle extends Shape {}
+class Triangle extends Shape {
+  draw() {
+    super.draw();
+    console.log('🔺');
+  }
+  getArea() {
+    return (this.width * this.heigth) / 2;
+  }
+
+
+  toString() {
+    return `Triangle: color: ${this.color}`;
+  }
+}
+
+const rectangle = new Rectangle(20, 20, 'blue');
+rectangle.draw();
+console.log(rectangle.getArea());
+const Triangle = new Triangle(20, 20, 'red');
+triangle.draw();
+console.log(triangle.getArea());
+
+
+// 6. Class checking: instanceeOf
+console.log(rectangle instanceof Rectangle);
+console.log(triangle instanceof Rectangle);
+console.log(triangle instanceof Triangle);
+console.log(triangle instanceof Shape);
+console.log(triangle instanceof Object);
+console.log(triangle.toString());
+console.log(triangle instanceof Rectangle);
