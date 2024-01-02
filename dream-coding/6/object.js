0624ecadaf3a89be7908161a53351d4544d1a9ca
이지윤 <ehfl226@gmail.com>
2024-01-02 23:33:10 +0900

    object

--- dream-coding/6/object.js
+++ dream-coding/6/object.js
@@ -44,3 +44,70 @@ console.log(yoon.name); // 코딩하는 순간 키에 해당하는 값을 받아
 console.log(yoon['name']); // 어떤 키가 필요한지 모를 때, 런타임때 결정
 yoon['hasJob'] = true; 
 console.log(yoon.hasJob);
+
+function printValue(obj, key) {
+    console.log(obj[key]);
+}
+printValue(yoon, 'name');
+printValue(yoon, 'age');
+
+// 3. Property value shorthand
+const person1 = { name: 'bob', age: 2 };
+const person2 = { name: 'steve', age: 3 };
+const person3 = { name: 'dave', age: 4 };
+const person4 = new Person('yoon', 30);
+console.log(person4);
+
+
+// 4. Constructor Function
+function Person(name, age) {
+    // this = {};
+    this.name = name;
+    this.age = age;
+    return this;
+}
+
+
+// 5. in operator: property existence check (key in obj)
+console.log('name' in yoon);
+console.log('age' in yoon);
+console.log('random' in yoon);
+console.log(yoon.random);
+
+
+// 6. for..in vs for..of
+// for (key in obj)
+console.clear();
+for (key in yoon) {
+    console.log(key);
+}
+
+// for (value of iterable)
+const array = [1, 2, 4, 5];
+for (value of array) {
+    console.log(value);
+}
+
+// 7. Fun cloning
+// Object.assingn(dest, [obj1, obj2, obj3...])
+const user = {name: 'yoon', age: '30' };
+const user2 = user;
+console.log(user);
+
+// old way
+const user3 = {};
+for (key in user) {
+    user3[key] = user[key];
+}
+console.clear();
+console.log(user3);
+
+const user4 = Object.assign({}, user);
+console.log(user4);
+
+// another example
+const fruit1 = { color: 'red' };
+const fruit2 = { color: 'blue', size: 'big' };
+const mixed = Object.assign({}, fruit1, fruit2);
+console.log(mixed.color);
+console.log(mixed.size);
